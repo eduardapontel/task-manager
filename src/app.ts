36d503e1204afa2f27e474423bac2e95e3954c05
Eduarda Pontel <eduardapontel@gmail.com>
2025-10-13 00:30:58 +0100

    feat: integrate Prisma for database management and add user creation endpoint

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,13 @@
 import express from 'express';
-import 'express-async-errors';
 
+import { routes } from './routes';
 import { errorHandler } from './middlewares/error-handler';
 
 const app = express();
+
 app.use(express.json());
+app.use(routes);
+
 app.use(errorHandler);
 
 export { app };
